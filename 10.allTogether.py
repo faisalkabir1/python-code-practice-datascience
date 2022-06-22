e876202ce7e0428694e6671723859c3b9c457254
Faisal Kabir <onlyfaisalkabir@gmail.com>
2022-06-22 22:09:57 +0600

    compliting all code

---
+++ 10.allTogether.py
@@ -0,0 +1,23 @@
+# heights and positions are available as lists
+
+# Import numpy
+import numpy as np
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+
+np_positions = np.array(positions)
+np_heights = np.array(heights)
+
+# Heights of the goalkeepers: gk_heights
+gk_heights = np_heights[np_positions == 'GK']
+
+# Heights of the other players: other_heights
+other_heights = np_heights[np_positions != 'GK']
+
+# Print out the median height of goalkeepers. Replace 'None'
+med_gk = np.median(gk_heights)
+print("Median height of goalkeepers: " + str(med_gk))
+
+# Print out the median height of other players. Replace 'None'
+med_other = np.median(other_heights)
+print("Median height of other players: " + str(med_other))
